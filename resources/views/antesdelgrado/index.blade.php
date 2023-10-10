2e2993564cbd6722272fff4f6e4b8ac3d7c8b5f2
xhlar <mauricio@xhlar.com>
2023-10-10 15:34:18 -0500

    arreglar formulario antes del grado index
    Signed-off-by: xhlar <mauricio@xhlar.com>

--- resources/views/antesdelgrado/index.blade.php
+++ resources/views/antesdelgrado/index.blade.php
@@ -32,7 +32,7 @@
     <select name="programa_cursado" id="programa_cursado" class="block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
         <option value="">Selecciona una carrera</option>
         @foreach($programas as $programa)
-            <option value="{{ $programa }}" {{ $formulario->programa_cursado == $programa ? 'selected' : '' }}>{{ $programa }}</option>
+            <option value="{{ $formulario->$programa }}" {{ $formulario->programa_cursado == $programa ? 'selected' : '' }}>{{ $programa }}</option>
         @endforeach
     </select>
 </div>
